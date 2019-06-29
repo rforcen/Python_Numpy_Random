51903a58472926b3dff4b4ea7acbc5af5872a40a
roberto forcen <robertoforcen@gmail.com>
2019-06-29 10:58:40 +0200

    Add files via upload

---
+++ randomMT/random.cpp
@@ -0,0 +1,105 @@
+/*
+    multithraded numpy random generator
+    runs 6 x times faster than numpy.random.random(size)
+    supports types: i1, u1, i2, u2, i4, u4, i8, u8, f4, f8
+
+    usage:
+    from RandomMT import RandomMT as rmt
+    ru = rmd.rand(1000, 'i2') # generate 1000 uint16 randoms
+*/
+
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <string.h>
+
+#include "Thread.h"
+
+namespace p = boost::python;
+namespace np = boost::python::numpy;
+using std::is_same;
+
+class RandomMT {
+    typedef np::ndarray (*RandFunc) (int n);
+
+public:
+     RandomMT() {
+        init();
+     }
+
+     static np::ndarray rand(int n, char* type) {
+        const char *types="i1i2u1u2u4u8i4i8f4f8";
+
+        const RandFunc fv[]=  // funcs matching 'types' array
+        {
+            rand_1_2<int8_t>,   rand_1_2<int16_t>,  rand_1_2<uint8_t>, rand_1_2<uint16_t>,
+            rand_4_8<uint32_t>, rand_4_8<uint64_t>, rand_4_8<int32_t>, rand_4_8<int64_t>,
+            rand_4_8<float>,    rand_4_8<double>
+        };
+        auto p=strstr(types, type);
+        if (p) return fv[(p-types)/2](n);
+
+        return rand_4_8<double>(n); // default
+     }
+
+private:
+     static bool checkType(const char*type, const char*tp) {
+         return !strncmp(type, tp, 2);
+     }
+     
+     template<typename T>
+     static np::ndarray rand_4_8(int n) { // 4, 8 bytes int float
+        T*rv=new T[n]; // random vector
+
+        T div = ((T)0.1==0) ? 1 : RAND_MAX; // is int?
+
+        Thread(n).run([&rv, div](int t, int from, int to) {
+            unsigned seed;
+            for (int i=from; i<to; i++)
+                rv[i]=(T)::rand_r(&seed) / div;
+        });
+
+        return np::from_data(rv,   // data -> rv
+                np::dtype::get_builtin<T>(),      // dtype -> T
+                p::make_tuple(n), // shape -> n
+                p::make_tuple(1*sizeof(T)), p::object());     // stride 1
+     }
+
+     template<typename T>
+     static np::ndarray rand_1_2(int n) { // 1, 2 bytes int
+        T*rv=new T[n];
+
+        T mod; // calc modulus to fit random values
+        if (is_same<T, uint8_t>::value)     mod=0xffu;
+        if (is_same<T, uint16_t>::value)    mod=0xffffu;
+        if (is_same<T, int8_t>::value)      mod=0x7f;
+        if (is_same<T, int16_t>::value)     mod=0x7fff;
+
+        Thread(n).run([&rv, mod](int t, int from, int to) {
+            unsigned seed;
+            for (int i=from; i<to; i++)
+                rv[i]=(T) (::rand_r(&seed) % mod);
+        });
+
+        return np::from_data(rv,   // data -> rv
+                np::dtype::get_builtin<T>(),      // dtype -> T
+                p::make_tuple(n), // shape -> n
+                p::make_tuple(1*sizeof(T)), p::object());     // stride 1
+     }
+
+     static unsigned init() {
+        Py_Initialize(); // init boost & numpy boost
+        np::initialize();
+        return 0;
+     }
+
+     static unsigned int junk;
+};
+
+unsigned RandomMT::junk=RandomMT::init(); // make sure init is executed
+
+BOOST_PYTHON_MODULE(RandomMT) {
+    p::class_<RandomMT>("RandomMT", p::init<>())
+        .def("rand", &RandomMT::rand);
+}
+
---
+++ randomMT/testRandom.py
@@ -0,0 +1,39 @@
+import numpy
+from RandomMT import RandomMT as rmt
+from timeit import default_timer as timer
+
+def compRandom():
+    print('random timing test...')
+    size = 1024 * 1024 * 32
+    tn = timer()
+    rndArr = numpy.random.random(size)
+    tn = timer() - tn
+    print('time numpy for ', size, 'values', tn)
+
+    tr = timer()
+    rndArr = rmt.rand(size, 'f8')
+    tr = timer() - tr
+    print('time RandomMT for ', size, 'values', tr, 'ratio numpy/mt:', tn/tr)
+
+    rndArr = rmt.rand(size, 'f8')
+    print('check rand-f8,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 1))
+    rndArr = rmt.rand(size, 'f4')
+    print('check rand-f4,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 1))
+    rndArr = rmt.rand(size, 'i1')
+    print('check rand-i1,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2 ** 7 - 1))
+    rndArr = rmt.rand(size, 'i2')
+    print('check rand-i2,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2 ** 15 - 1))
+    rndArr = rmt.rand(size, 'u1')
+    print('check rand-u1,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2 ** 8 - 1))
+    rndArr = rmt.rand(size, 'u2')
+    print('check rand-u2,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2 ** 16 - 1))
+    rndArr = rmt.rand(size, 'i4')
+    print('check rand-i4,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2 ** 31 - 1))
+    rndArr = rmt.rand(size, 'i8')
+    print('check rand-i8,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2**64-1))
+    rndArr = rmt.rand(size, 'u4')
+    print('check rand-u4,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2 ** 32 - 1))
+    rndArr = rmt.rand(size, 'u8')
+    print('check rand-u8,', numpy.all(rndArr >= 0) and numpy.all(rndArr <= 2**64-1))
+
+compRandom()
